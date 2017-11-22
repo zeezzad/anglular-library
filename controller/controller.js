b3232ac2433287ec65069475f7efd059f08a8e05
zeezzad <panon508@gmail.com>
2017-11-22 15:47:41 +0700

    version ang

---
+++ controller/controller.js
@@ -0,0 +1,7 @@
+var myApp = angular.module('myApp', []);
+
+        myApp.controller('myController', function ($scope) {
+            $scope.myArr = ['Steve', 'Bill', 'James', 'Rob', 'Ram', 'Moin']
+        });
+
+        
