273bf9968e661317c9c6acdb4041facc97295ecb
zeezzad <panon508@gmail.com>
2017-11-27 10:19:38 +0700

    insert v2

--- insert.php
+++ insert.php
@@ -1,5 +1,27 @@
 <?php
 
+$postdata = file_get_contents(php://input);
+$request  = json_decode($postdata);
+$username = $request->username;
+$password = $request->password;
 
+$sername = "localhost";
+$user    = "root";
+$pass    = "";
+$db      = "javawebmvc";
 
+$conn = new mysqli($sername,$user,$pass,$db);
+
+if ($conn->connect_error) {
+    die("Connect Error: " .$conn->connect_error);
+}
+
+$sql = "insert into user(username,password)values($username,$password)";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
