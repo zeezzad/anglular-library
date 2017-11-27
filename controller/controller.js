3252f6e7ecaaf59ed1321f382c6951ad7969769d
zeezzad <panon508@gmail.com>
2017-11-27 10:03:37 +0700

    insert data

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,24 @@
 var myApp = angular.module('myApp', []);
-     myApp.controller("myController", function ($scope) {
-        $scope.message = "Hello Angular World!";
+
+myApp.controller("empcontroller", function ($scope $http) {    
+       
+$scope.postData = function(){
+
+      var request = $http({
+       
+          method: "post",
+          url   :  window.location.href +"insert.php",
+          data :{
+             username: $scope.username,
+             password: $scope.password,
+
+          },
+
+          headers:{'Content-Type': 'application/x-www-form-urlencoded'}
+      });
+
+    }
+
     });
     
         
